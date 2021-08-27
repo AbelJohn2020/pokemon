86ddc3d78a73d79defc52beae4d53103a954416c
Abel AR <abel_john_14@hotmail.com>
2021-08-27 10:22:18 -0500

    Get new array of information pokemon

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -29,6 +29,32 @@ const Pokemon = ({id, url, name, picture}: propsData) => {
             .finally(() => setIsLoading(false))
     }, []);
 
+    const informationCard = () => {
+        if(dataPokemon) {
+            const getId = dataPokemon.id;
+            const getAbilities = dataPokemon.abilities.map( ability => ability.ability.name);
+            const getExperience = dataPokemon.base_experience;
+            const getheight = dataPokemon.height;
+            const getMoves = dataPokemon.moves.map( movements => movements.move.name);
+            const getTypes = dataPokemon.types.map( species => species.type.name);
+            const getWeight = dataPokemon.weight;
+
+            return {
+                id: getId,
+                abilities: getAbilities,
+                experience: getExperience,
+                height: getheight,
+                moves: getMoves,
+                type: getTypes,
+                weight: getWeight,
+            }
+        } else {
+            return dataPokemon;
+        }
+    }
+
+    const information = informationCard();
+
     return (
         <div>
             {
